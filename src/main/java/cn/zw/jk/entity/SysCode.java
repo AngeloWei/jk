d911ef488a304e5431de673493bf173bb568d386
zw <zw@zwhp>
2017-12-28 18:36:45 +0800

    增加 附件新增

---
+++ src/main/java/cn/zw/jk/entity/SysCode.java
@@ -0,0 +1,22 @@
+package cn.zw.jk.entity;
+
+public class SysCode {
+    private String sysCodeId;
+    private String name;
+
+    public String getSysCodeId() {
+        return sysCodeId;
+    }
+
+    public void setSysCodeId(String sysCodeId) {
+        this.sysCodeId = sysCodeId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
